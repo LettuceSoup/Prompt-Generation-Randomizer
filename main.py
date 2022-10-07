2c9108452af5177853537765f3ad913e19200329
Kaylee Crow <113363364+LettuceSoup@users.noreply.github.com>
2022-10-07 18:21:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from random import choice, random
+
+# WELCOME!!!
+print("┏━ •❃°•°❀°•°❃• ━┓")
+print("❀ cute prompts  ❀")
+print("┗━ •❃°•°❀°•°❃• ━┛")
+print(" ")
+
+# OCS!!! 
+name = ["TP", "Addicus", "Ezra", "Elijah", "Ren", "Desmond", "Pierre", "Kash", "Riku", "Evic", "Vian", "Royale", "Valen", "Gage", "Asterite", "Salem", "Quincy", "Kenzie", "Milo", "Elio", "Dicer", "Asarath", "Lyien", "Mikai", "Maka", "Octavious", "Elias", "Kaivon", "Lawliet", "Yohan", "Xavian", "Everette", "Jupiter", "Harley", "Skylar", "Ashton", "Zacharias", "Casper", "Vance", "Vyn", "Caspian", "Tracker", "Kaiser", "Tobias"]
+        
+print(" ✧.* OC:", (choice(name)))
+print(" ")
+
+# ACTIVITY PROMPTS!!! 
+activity = ["Listening to music", "Reading a book", "Snuggling/Cuddling", "Singing/humming", "Dancing", "Napping", "Asleep", "Sewing", "Drawing", "Cooking", "Driving", "Looking out the Window", "Baking", "Playing an instrument", "With their weapon", "Trying on new clothes", "Crying", "Smiling", "Laughing", "Angry", "Sorry", "Apologetic", "In Love", "Hatred for (something)", "With an animal" "As an animal", "Outfit swap", ]
+
+print(" ✧.* Prompt:", (choice(activity)))
+print(" ")
+
+# EMOTIONS!!
+# emotion = ["Tired", "Happy", "Sad", "Relaxed", "Pissed", ]
